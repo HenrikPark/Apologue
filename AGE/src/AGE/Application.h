9363687177ccbb1dbb51f6a7b092fe07808b0bf5
HenrikPark <Henriksvensson664@gmail.com>
2022-07-04 10:22:38 +0200

    Added a OpenGL shader.

--- AGE/src/AGE/Application.h
+++ AGE/src/AGE/Application.h
@@ -9,6 +9,8 @@
 
 #include "AGE/ImGui/ImGuiLayer.h"
 
+#include "AGE/Renderer/Shader.h"
+
 
 namespace AGE
 {
@@ -36,6 +38,10 @@ namespace AGE
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
+		std::unique_ptr<Shader> m_Shader;
 
 		static Application* s_Instance;
 	};
--- AGE/src/Platform/OpenGl/OpenGLContext.cpp
+++ AGE/src/Platform/OpenGl/OpenGLContext.cpp
@@ -16,6 +16,11 @@ namespace AGE
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		AGE_CORE_ASSERT(status, "Failed to initialize Glad!");
+			
+		AGE_CORE_INFO("OpenGL Info:");
+		AGE_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		AGE_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		AGE_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
