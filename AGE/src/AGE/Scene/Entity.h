62e3e10a47a4320c64f17117214d043136392fe8
HenrikPark <Henriksvensson664@gmail.com>
2022-07-31 11:54:52 +0200

    added add and remove Entity and components.

--- AGE/src/AGE/Scene/Entity.h
+++ AGE/src/AGE/Scene/Entity.h
@@ -16,7 +16,9 @@ namespace AGE
 		T& AddComponent(Args&&... args)
 		{
 			AGE_CORE_ASSERT(!HasComponent<T>(), "Entity already has component");
-			return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			m_Scene->OnComponentAdded<T>(*this, component);
+			return component;
 		}
 
 		template <typename T>
@@ -33,13 +35,13 @@ namespace AGE
 		}
 
 		template <typename T>
-		void RemoveComponen()
+		void RemoveComponent()
 		{
 			m_Scene->m_Registry.remove<T>(m_EntityHandle);
 		}
 
 		operator bool() const { return m_EntityHandle != entt::null; }
-		
+		operator entt::entity() const { return m_EntityHandle; }
 		operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 
 		bool operator==(const Entity& other) const
--- AGE/src/AGE/Scene/Scene.cpp
+++ AGE/src/AGE/Scene/Scene.cpp
@@ -25,6 +25,11 @@ namespace AGE
 		return entity;
 	}
 
+	void Scene::DestroyEntity(Entity entity)
+	{
+		m_Registry.destroy(entity);
+	}
+
 
 	void Scene::OnUpdate(Timestep DeltaTime)
 	{
@@ -96,5 +101,35 @@ namespace AGE
 
 	}
 
+	template<typename T>
+	void Scene::OnComponentAdded(Entity entity, T& component)
+	{
+		static_assert(false);
+	}
 
+	template<>
+	void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component)
+	{
+		component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<NativeScriptComponent>(Entity entity, NativeScriptComponent& component)
+	{
+	}
 }
