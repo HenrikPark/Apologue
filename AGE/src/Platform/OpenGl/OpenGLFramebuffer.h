20bca79b16dae3586c9e9345e7e3365de31dda9f
HenrikPark <Henriksvensson664@gmail.com>
2022-08-03 12:41:24 +0200

    Prepared for mouse picking.

--- AGE/src/Platform/OpenGl/OpenGLFramebuffer.h
+++ AGE/src/Platform/OpenGl/OpenGLFramebuffer.h
@@ -16,12 +16,17 @@ namespace AGE
 
 		virtual void Resize(uint32_t width, uint32_t height) override;
 
-		virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+		virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override {AGE_CORE_ASSERT(index < m_ColorAttachments.size(), "m_ColorAttachemnt is less than Index"); return m_ColorAttachments[index]; }
 
 		virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
 	private:
-		uint32_t m_RendererID = 0;
-		uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
+		uint32_t m_RendererID = 0;		
 		FramebufferSpecification m_Specification;
+
+		std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+		FramebufferTextureSpecification m_DepthAttachmentSpecification = FramebufferTextureFormat::None;
+
+		std::vector<uint32_t> m_ColorAttachments;
+		uint32_t m_DepthAttachment = 0;
 	};
 }
