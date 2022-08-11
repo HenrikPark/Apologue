ad6bed525ba0cb638a02692530f1842adc185b23
HenrikPark <Henriksvensson664@gmail.com>
2022-08-11 11:46:31 +0200

    load scenes by drag n drop.

--- AGE-Editor/src/EditorLayer.h
+++ AGE-Editor/src/EditorLayer.h
@@ -27,6 +27,7 @@ namespace AGE
 
 		void NewScene();
 		void OpenScene();
+		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
 	private:
 		OrthographicCameraController m_CameraController;
