347388f02878279dac34a86fa09153806a016849
HenrikPark <Henriksvensson664@gmail.com>
2022-08-18 09:02:44 +0200

    Added Rectangles and lines

--- AGE/src/AGE/Scene/Scene.cpp
+++ AGE/src/AGE/Scene/Scene.cpp
@@ -272,6 +272,11 @@ namespace AGE
 			}
 		}
 
+		//Draw Line
+		//Renderer2D::SetLineWidth(2.0f);
+		//Renderer2D::DrawLine(glm::vec3(2.0f), glm::vec3(5.0f), glm::vec4(0, 0, 1, 1));
+		//Renderer2D::DrawRect(glm::vec3(0.0f), glm::vec2(1.0f), glm::vec4(1, 0, 0, 1));
+
 		Renderer2D::EndScene();
 	}
 
--- AGE/src/Platform/OpenGl/OpenGLRendererAPI.h
+++ AGE/src/Platform/OpenGl/OpenGLRendererAPI.h
@@ -13,6 +13,8 @@ namespace AGE
 		virtual void Clear() override;
 
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
+		virtual void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount) override;
 
+		virtual void SetLineWidth(float width) override;
 	};
 }
